78b990a7171cd5d19bb7db9eb7222df31ab32f3c
Josh Paul <joshua.paul@uconn.edu>
2014-04-23 21:40:50 -0700

    add a method to the post object to calculate time since the post was created.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,33 @@
 class Post < ActiveRecord::Base
 
   belongs_to :user
+
+
+
+  def how_long
+  	origin = self.created_at
+  	age = Time.now - origin
+  	age = age.to_i
+  	if age < 3600
+  		age = age/60
+  		return "#{age.round} minutes ago"
+  	elsif age >= 3600 && age < 86400
+  		age = age/3600
+  		return "#{age} hours ago"
+  	elsif age >= 86400 && age <604800
+  	    age = age/86400
+  	    return "#{age} days ago"
+  	elsif age >= 604800 && age < 2419200
+  		age = age/604800 
+  		return "#{age} weeks ago"
+    elsif age >= 2419200 && age < 1.year
+    	age = age/2419200
+    	return "#{age} months ago"
+    else
+    	return "#{age} more than a year ago"
+    end
+  end
 end
+    	
+  		
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def greeting
+    if Time.now.hour >= 17
+      return "Good Evening"
+    Time.now.hour > 12
+      return "Good Afternoon"
+    else
+      return "Good Morning"
+    end
+  end
 end
